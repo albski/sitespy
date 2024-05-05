c45fad209484b413aaa004c0dd58b7489464059d
Albert Skonieczny <albertskonieczny@gmail.com>
2024-05-05 15:23:27 +0200

    Config as non-dataclass

--- sitespy/config.py
+++ sitespy/config.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass, field, asdict
+from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+from typing import Any, List, Self
 from sitespy import telegram as telegram_class
 from sitespy import utils
 from json import load as json_load
@@ -19,53 +19,68 @@ class SpyEntry:
     img_path: Path
 
 
-@dataclass
 class Config:
-    _telegram: telegram_class.Telegram
-    _spy_entries: List[SpyEntry]
-    _subscribers: List[asyncio.Queue] = field(
-        default_factory=list,
-        init=False,
-        repr=False,
-    )
+    def __init__(self):
+        self.__telegram: telegram_class.Telegram
+        self.__spy_entries: List[SpyEntry]
+        self.__subscribers: List[asyncio.Queue] = []
+
+    @classmethod
+    def from_dict(cls, dict_from_json: dict[Any, Any]) -> Self:
+        instance = cls()
+
+        instance.__telegram = telegram_class.Telegram(placeholder="123")
+
+        instance.__spy_entries = [
+            SpyEntry(
+                url=entry["url"],
+                interval_seconds=entry["interval_seconds"],
+                img_path=Path(entry["img_path"]),
+            )
+            for entry in dict_from_json.get("spy_entries", [])
+        ]
+
+        return instance
+
+    def to_dict(self) -> dict[Any, Any]:
+        return {
+            "telegram": {"placeholder": "123"},
+            "spy_entries": [
+                {
+                    "url": entry.url,
+                    "interval_seconds": entry.interval_seconds,
+                    "img_path": entry.img_path.as_posix(),
+                }
+                for entry in self.__spy_entries
+            ],
+        }
 
     def notify(self):
-        for queue in self._subscribers:
+        for queue in self.__subscribers:
             queue.put_nowait("UPDATED")
 
     def subscribe(self, subscriber):
-        self._subscribers.append(subscriber)
+        self.__subscribers.append(subscriber)
 
     def unsubscribe(self, subscriber):
-        self._subscribers.remove(subscriber)
+        self.__subscribers.remove(subscriber)
 
     @property
-    def telegram(self):
-        return self._telegram
+    def telegram(self) -> telegram_class.Telegram:
+        return self.__telegram
+
+    @telegram.setter
+    def telegram(self, value: telegram_class.Telegram):
+        self.__telegram = value
+        self.notify()
 
     @property
-    def spy_entries(self):
-        return self._spy_entries
+    def spy_entries(self) -> List[SpyEntry]:
+        return self.__spy_entries
 
-    def set_telegram(self, telegram: telegram_class.Telegram):
-        self._telegram = telegram
-        self.notify()
-
-    def add_spy_entry(self, spy_entry: SpyEntry):
-        self._spy_entries.append(spy_entry)
-        self.notify()
-
-    def remove_spy_entry(self, spy_entry: SpyEntry):
-        self._spy_entries.remove(spy_entry)
-        self.notify()
-
-    def update_spy_entry(self, old_spy_entry: SpyEntry, new_spy_entry: SpyEntry):
-        self._spy_entries.remove(old_spy_entry)
-        self._spy_entries.append(new_spy_entry)
-        self.notify()
-
-    def clear_spy_entries(self):
-        self._spy_entries.clear()
+    @spy_entries.setter
+    def spy_entries(self, value: List[SpyEntry]):
+        self.__spy_entries = value
         self.notify()
 
 
@@ -87,6 +102,8 @@ class ConfigManager:
         self.path_manager = path_manager
         self.config.subscribe(self.update)
 
+        # TODO: handle cases where json (self.path_manager.config_file) has not been initialized or it is not compilant to Config class
+
         try:
             self.open = open(self.path_manager.config_file, "r+")
             self.json = json_load(self.open)
@@ -94,13 +111,14 @@ class ConfigManager:
             print(f"Failed to open or parse the config file: {error}")
             self.open = None
             self.json = None
+            # TODO: handle cases when those are None
 
         self.__initialized = True
 
     async def update(self, message):
-        if message == "UPDATED":
-            with open(self.path_manager.config_file, "w") as f:
-                json_dump(asdict(self.config), f)
+        if message == "UPDATED" and self.open is not None:
+            self.open.seek(0)
+            json_dump(self.config.to_dict(), self.open, indent=4)
 
     def __enter__(self):
         return self
@@ -109,6 +127,25 @@ class ConfigManager:
         if self.open is not None:
             self.open.close()
 
+        self.config.unsubscribe(self.update)
+
 
 if __name__ == "__main__":
-    pass
+    from sitespy.utils import PathManager, Platform
+    import sys
+
+    loop = asyncio.get_event_loop()
+
+    platform = Platform.from_string(sys.platform)
+    path_manager = PathManager(platform)
+
+    with open(path_manager.config_file, "r") as file:
+        dict_from_json = json_load(file)
+
+    config = Config.from_dict(dict_from_json)
+    config_manager = ConfigManager(config, path_manager)
+
+    async def simulate_config_update():
+        await asyncio.sleep(5)
+
+    loop.run_until_complete(simulate_config_update())
