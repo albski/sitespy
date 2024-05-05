3e8211a4275c4f4f7dc70bc66efdca851e0ee4df
Albert Skonieczny <albertskonieczny@gmail.com>
2024-05-05 21:24:41 +0200

    Fix Pub/Sub in config module

--- sitespy/config.py
+++ sitespy/config.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Any, List
+from typing import Any, List, Callable
 from json import load as json_load
 from json import dump as json_dump
 from sys import exit as sys_exit
@@ -20,22 +20,30 @@ class SpyEntry:
 
 
 @dataclass
-class ConfigData:
-    """ConfigData represents a structure of data in config.json."""
+class Config:
+    """Config represents a data structured in config.json."""
 
     telegram: Telegram = field(default_factory=lambda: Telegram(placeholder=""))
     spy_entries: List[SpyEntry] = field(default_factory=list)
 
 
-class Config:
+class ConfigManager:
     """
-    It acts as a publisher to ConfigManager and other subscribers.
-    The class publishes "UPDATED" when change in config occured.
+    ConfigManager manages changes in config in cache.
+    It is a publisher to Callable objects.
     """
 
     def __init__(self):
-        self.__config_data: ConfigData = ConfigData()
-        self.__subscribers: List[asyncio.Queue] = []
+        self.__config_data = Config()
+        self.__subscribers: List[Callable] = []
+        self.__loop = asyncio.get_running_loop()
+
+    def subscribe(self, callback):
+        self.__subscribers.append(callback)
+
+    def notify(self):
+        for subscriber in self.__subscribers:
+            self.__loop.create_task(subscriber())
 
     def to_dict(self) -> dict[Any, Any]:
         return {
@@ -49,16 +57,6 @@ class Config:
                 for entry in self.__config_data.spy_entries
             ],
         }
-
-    def notify(self):
-        for queue in self.__subscribers:
-            queue.put_nowait("UPDATED")
-
-    def subscribe(self, subscriber):
-        self.__subscribers.append(subscriber)
-
-    def unsubscribe(self, subscriber):
-        self.__subscribers.remove(subscriber)
 
     @property
     def telegram(self) -> Telegram:
@@ -79,27 +77,29 @@ class Config:
         self.notify()
 
 
-class ConfigManager:
+class ConfigHandler:
     """
-    ConfigManager class handles all intermediaries between config in cache and config.json.
+    ConfigHandler handles all intermediaries between config in cache and config.json.
     """
 
     __instance = None
 
     def __new__(cls, *args, **kwargs):
+        """Singleton boilerplate."""
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
             cls.__initialized = False
             return cls.__instance
         return cls.__instance
 
-    def __init__(self, config: Config, path_manager: utils.PathManager):
+    def __init__(self, config: ConfigManager, path_manager: utils.PathManager):
         if self.__initialized:
             return
 
         self.config = config
         self.path_manager = path_manager
-        self.config.subscribe(self.update)
+
+        self.config.subscribe(self.update_config)
 
         self.init_config_json()
 
@@ -130,10 +130,14 @@ class ConfigManager:
         # TODO: validate config
         pass
 
-    async def update(self, message):
-        if message == "UPDATED" and self.open is not None:
+    async def update_config(self):
+        """Asynchronously update the config file upon notification."""
+        await asyncio.sleep(0)
+        if self.open:
             self.open.seek(0)
+            self.open.truncate()
             json_dump(self.config.to_dict(), self.open, indent=4)
+            self.open.flush()
 
     def __enter__(self):
         return self
@@ -142,30 +146,21 @@ class ConfigManager:
         if self.open is not None:
             self.open.close()
 
-        self.config.unsubscribe(self.update)
-
 
 if __name__ == "__main__":
     from sitespy.utils import PathManager, Platform
     import sys
 
-    loop = asyncio.get_event_loop()
+    async def main():
+        platform = Platform.from_string(sys.platform)
+        path_manager = PathManager(platform)
 
-    platform = Platform.from_string(sys.platform)
-    path_manager = PathManager(platform)
+        config = ConfigManager()
+        ConfigHandler(config, path_manager)
+        config.telegram = Telegram(placeholder="New Placeholder")
+        print("Config updated.")
+        await asyncio.sleep(2)
+        config.telegram = Telegram(placeholder="xd")
+        await asyncio.sleep(3)
 
-    with open(path_manager.config_file, "r") as file:
-        dict_from_json = json_load(file)
-
-    config = Config()
-    config_manager = ConfigManager(config, path_manager)
-    print(config.telegram)
-
-    async def simulate_config_update():
-        config.telegram = Telegram("123")
-        await asyncio.sleep(1)
-        print(config.telegram)
-
-    loop.run_until_complete(simulate_config_update())
-    while True:
-        pass
+    asyncio.run(main())
